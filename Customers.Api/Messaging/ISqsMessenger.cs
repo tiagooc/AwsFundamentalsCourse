9a54308eeb67be223e68f8781c3383db9661a129
Tiago Costa <phb.tiago@gmail.com>
2023-09-04 21:06:52 +0100

    feat: Init repo

---
+++ Customers.Api/Messaging/ISqsMessenger.cs
@@ -0,0 +1,8 @@
+﻿namespace Customers.Api.Messaging;
+
+using Amazon.SQS.Model;
+
+public interface ISqsMessenger
+{
+    Task<SendMessageResponse> SendMessageAsync<T>(T message);
+}
---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customerCreated = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "email@email.com",
+    FullName = "User Full Name",
+    DateOfBirth = new DateTime(1991, 1, 1),
+    GitHubUsername = "githubUsername"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customerCreated),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>()
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
